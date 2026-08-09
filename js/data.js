070c4a1bf104c72a28bc1dc0538e0d37c2452cb8
amir <amiralsharabi2@gmail.com>
2026-08-10 01:23:09 +0300

    feat: add the information of the game and player

---
+++ js/data.js
@@ -0,0 +1,40 @@
+// board size
+const boardSize = 10;
+
+// board data
+const water = 0;
+const ship = 1;
+const hit = 2;
+const miss = 3;
+
+// ship data
+const shipData = [
+  { name: "Carrier", size: 5 },
+  { name: "lion", size: 4 },
+  { name: "Cruiser", size: 3 },
+  { name: "Submarine", size: 3 },
+  { name: "Destroyer", size: 2 },
+];
+
+//who's turn
+const player1 = 1;
+const player2 = 2;
+
+//game status (phase)
+const gamePhase = {
+  setup: 1,
+  playing: 2,
+  gameOver: 3,
+};
+
+// player board data
+const player1Board = [];
+const computerBoard = [];
+
+// player ship data
+const player1Ships = [];
+const computerShips = [];
+
+//
+
+
