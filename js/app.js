972dd8575eb6052f32fedb664dff9ff5a5fc066c
amir <amiralsharabi2@gmail.com>
2026-08-10 19:47:43 +0300

    feat: adding the get elements from hhtml in app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+//get elements
+const playerBoardEl = document.querySelector('#player-board');
+const computerBoardEl = document.querySelector('#computer-board');
+const rotateBtn = document.getElementById('rotate');
+const shipDockEl = document.getElementById('ship-dock');
+const playAgainBtn = document.getElementById('play-again');
 // function that turns the array into 100 divs on screen
 
 function renderBoard()  {
