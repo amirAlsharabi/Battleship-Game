247dd89d7cd2e73af84c0ffb266740b60785acff
amir <amiralsharabi2@gmail.com>
2026-08-11 20:16:52 +0300

    feat: create the function for place ships randomly

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,43 @@
 function isValidPlacement(board, row, col, size, direction) {
+  let checkRow;
+  let checkCol;
+  for (let i = 0; i < boardSize; i++) {
+    if (direction === "horizontal") {
+      checkRow = row + i;
+    } else if (direction === "vertical") {
+      checkCol = col + i;
+    } else if (checkCol > boardSize || checkRow > boardSize) {
+      return false;
+    } else if (board.row.index === ship && board.col.index === ship) {
+      return false;
+    }
+  }
+
   return true;
+}
+
+function placeShipRandomly(board, ship) {
+     let placed = false;
+  while (!placed) {
+   let col =Math.floor(Math.random() * boardSize);
+  let row =Math.floor(Math.random() * boardSize);
+  direction = Math.floor(Math.random() * 0.5  ? 'horizontal' : 'vertical' )
+
+  if(isValidPlacement(board,row,col,ship.size,direction) === true){
+    ship.cell=[];
+
+    for(let i =0 ; i < ship.size ; i++){
+        let placeRow=row;
+        let placeCol=col;
+        board[placeRow][placeCol]=ship 
+        ship.cell.push([placeRow,placeCol])
+        ship.placed=true;
+    }
+
+  }
+ship.placed = true;
+  placed= true;
+
+  }
+
 }
